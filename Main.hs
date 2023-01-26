01e10570fd444be9675654a5cc9a53f99b92e07d
Strke12i <felipebagesteiro@gmail.com>
2023-01-26 17:12:03 -0300

    adicionando algumas funcionalidades em haskell

--- Main.hs
+++ Main.hs
@@ -6,20 +6,20 @@ svgBegin w h = printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/
 svgEnd :: String
 svgEnd = "</svg>"
 
-svgPolygon :: Int -> (Int, Int) -> (Int,Int) -> (Int,Int) -> (Int, Int) -> (Int, Int) -> Int -> String
-svgPolygon x p1 p2 p3 p4 p5 y = 
-    printf "<polygon points ='%d ,%d %d, %d %d, %d %d,%d %d, %d %d, %d' style='fill:lime;stroke:purple;stroke-width:5;fill-rule:nonzero;' ></polygon>" x (fst p1) (snd p1) (fst p2) (snd p2) (fst p3) (snd p3) (fst p4) (snd p4) (fst p5) (snd p5) y
+svgPolygon :: Float -> Float -> Float -> Float -> Float -> Float -> String
+svgPolygon x y width height radius border = 
+    printf "<path d='M %.4f %.4f a %.4f %.4f 0 0 1 %.4f 0 l %.4f %.4f a %.4f %.4f 0 0 1 %.4f %.4f l 0 %.4f a %.4f %.4f 0 0 1 %.4f %.4f l %.4f %.4f a %.4f %.4f 0 0 1 %.4f 0 l %.4f %.4f a %.4f %.4f 0 0 1 %.4f %.4f l 0 %.4f a %.4f %.4f 0 0 1 %.4f %.4fz' style='fill:black;stroke:yellow;stroke-width:%.4f;fill-rule:nonzero;stroke-linejoin:round;stroke-linecap:round;'/>\n"
+    x y radius radius radius
+    width height radius radius (radius/2) (radius*0.866)
+    (height *2) radius radius (-radius/2) (radius *0.866)
+    (-width) height radius radius (-radius)
+    (-width) (-height) radius radius (-radius/2) ((-radius/2 ) * 0.866)
+    (-height * 2) radius radius (-radius/2) ((-radius/2 ) * 0.866) border
+
 
 svgAll :: String 
 svgAll = svgBegin 1920 1080 ++
--- x(largura)
--- p1 ( vertice direita lateral cima) 
--- p2 ( vertice direita lateral baixo)
--- p3 ( vertice baixo)
--- p4 (vertice esquerda lateral baixo)
--- p5 (vertice esquerda lateral cima)
-    svgPolygon 300 (150,225) (280,75) (280,0) (150,75) (20,225) 20 ++
-    svgPolygon 300 (150,525) (280,75) (280,0) (150,75) (20,225) 20 ++
+    svgPolygon 150 30 100 50 5 10 ++
     svgEnd
 
 main ::IO ()
